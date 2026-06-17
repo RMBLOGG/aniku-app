09126ac6db17b99e5bb87fb36c6fc0700bd556cb
RMBLOGG <ramdanhidayah176@gmail.com>
2026-06-18 04:10:07 +0700

    redesain

--- app/src/main/java/com/example/ui/Screens.kt
+++ app/src/main/java/com/example/ui/Screens.kt
@@ -41,6 +41,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
