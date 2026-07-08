cc726a2d7972e9f9d70f38562965e5e0d144ebc6
RMBLOGG <ramdanhidayah176@gmail.com>
2026-07-08 17:12:06 +0700

    add preview

--- app/src/main/java/com/example/ui/theme/ComicFonts.kt
+++ app/src/main/java/com/example/ui/theme/ComicFonts.kt
@@ -1,9 +1,9 @@
 package com.example.ui.theme
 
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import com.example.R
 
