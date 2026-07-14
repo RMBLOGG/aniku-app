1ad76403f56e31caf1fdb15295df35e023207c64
RMBLOGG <ramdanhidayah176@gmail.com>
2026-07-15 01:53:15 +0700

    add preview

--- app/src/main/java/com/example/ui/QuizScreen.kt
+++ app/src/main/java/com/example/ui/QuizScreen.kt
@@ -2,6 +2,7 @@ package com.example.ui
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.*
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
