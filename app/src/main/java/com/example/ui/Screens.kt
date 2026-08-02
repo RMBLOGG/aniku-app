949a2b1a3425d1455e211decec9b195362fbf022
RMBLOGG <ramdanhidayah176@gmail.com>
2026-08-02 07:41:33 +0700

    add preview

--- app/src/main/java/com/example/ui/Screens.kt
+++ app/src/main/java/com/example/ui/Screens.kt
@@ -10807,7 +10807,7 @@ fun SumberDataScreen(
                 Triple("Dayynime-v2", "Sumber alternatif (server 2)", false),
                 Triple("Dayynime-v3", "Animekompi (server 3)", false),
                 Triple("Dayynime-v4", "Donghua (server 4)", false),
-                Triple("Dayynime-v5", "Streaming lancar & loading cepat (server 5)", true)
+                Triple("Dayynime-v5", "Streaming lancar & loading cepat (server 5)", false)
             )
 
             sources.forEach { (sourceKey, sourceDesc, premiumOnly) ->
