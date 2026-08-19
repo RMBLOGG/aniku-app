d19c39f69069622e8343e10f95ac6f7c3588af65
RMBLOGG <ramdanhidayah176@gmail.com>
2026-08-20 05:07:28 +0700

    add preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "com.aistudio.aniku.plxwzq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 26
-    versionName = "1.4.8"
+    versionCode = 27
+    versionName = "1.4.9"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
